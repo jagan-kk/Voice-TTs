534bc448ff7a0ad3427b2c2b91b40b80b72f270c
Jagan K K <jagankk00723@gmail.com>
2026-09-16 19:39:12 +0530

    speech-to-text

---
+++ voice_agent.py
@@ -0,0 +1,29 @@
+import sounddevice as sd
+from faster_whisper import WhisperModel
+
+model = WhisperModel("small",device="cpu", compute_type="int8")
+
+SAMPLE_RATE = 16000
+DURATION = 10
+
+print("Speak...")
+
+audio = sd.rec(
+    int(DURATION * SAMPLE_RATE),
+    samplerate=SAMPLE_RATE,
+    channels=1,
+    dtype="float32"
+)
+
+sd.wait()
+
+segments, info = model.transcribe(
+    audio[:, 0],
+    language="en",
+    vad_filter=True,
+    beam_size=5
+    )
+
+text = "".join(segment.text for segment in segments).strip()
+
+print("You said:", text)
